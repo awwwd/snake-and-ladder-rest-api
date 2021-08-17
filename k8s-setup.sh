d92d7a1784922e133f75231e401bfc12d155206e
Amit Auddy <amitauddy94@gmail.com>
2021-08-17 09:34:09 +0530

    Added mongo as sts

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -2,15 +2,15 @@
 
 case "$1" in
   deploy)
-    echo "[INFO] Deploying mongodb in kubernetes..."
-    kubectl apply -f deployment/db/deployment.yml
-    sleep 10
+    echo "[INFO] Deploying mongodb in kubernetes as statefulset..."
+    kubectl apply -f deployment/db/statefulset.yml
+    sleep 5
     echo "[INFO] Deploying app in kubernetes..."
     kubectl apply -f deployment/app/deployment.yml
   ;;
   destroy)
-    echo "[INFO] Deleting mongodb from kubernetes..."
-    kubectl delete -f deployment/db/deployment.yml
+    echo "[INFO] Deleting mongodb sts from kubernetes..."
+    kubectl delete -f deployment/db/statefulset.yml
     echo "[INFO] Deleting app from kubernetes..."
     kubectl delete -f deployment/app/deployment.yml
   ;;
